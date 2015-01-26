94478806a26f82328220570af30a9eb4e72bff75
Ryota Kameoka <kameoka.ryota@gmail.com>
2015-01-26 15:25:01 +0900

    Implement Tab::AutoReloadable module

---
+++ tab/auto_reloadable.rb
@@ -0,0 +1,12 @@
+module Tab
+  module AutoReloadable
+    def auto_reload(period, &block)
+      Thread.new do
+        loop do
+          sleep period
+          block.call
+        end
+      end
+    end
+  end
+end
