ab3baa12dc4d02f5e09e6802d294cbeb988fd496
Ryota Kameoka <kameoka.ryota@gmail.com>
2015-03-17 04:28:18 +0900

    Require Launchy

--- lib/twterm.rb
+++ lib/twterm.rb
@@ -3,6 +3,7 @@ $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname
 
 require 'cgi'
 require 'curses'
+require 'launchy'
 require 'oauth'
 require 'readline'
 require 'singleton'
