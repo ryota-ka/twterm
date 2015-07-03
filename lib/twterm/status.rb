84066beacc46528a8e3cd070975a3c929a4a095e
Ryota Kameoka <kameoka.ryota@gmail.com>
2015-07-04 04:47:02 +0900

    Sort methods in Status class in alphabetical order

--- lib/twterm/status.rb
+++ lib/twterm/status.rb
@@ -2,15 +2,51 @@ module Twterm
   class Status
     MAX_CACHED_TIME = 3600
 
-    attr_reader :id, :text, :created_at, :created_at_for_sort, :retweet_count, :favorite_count, :in_reply_to_status_id, :favorited, :retweeted, :user_id, :retweeted_by_user_id, :urls, :media, :touched_at
+    attr_reader :created_at, :created_at_for_sort, :favorite_count, :favorited,
+      :id, :in_reply_to_status_id, :media, :retweet_count,
+      :retweeted_by_user_id, :text, :touched_at, :urls, :user_id
     alias_method :favorited?, :favorited
     alias_method :retweeted?, :retweeted
 
     @@instances = {}
 
-    def self.new(tweet)
-      instance = find(tweet.id)
-      instance.nil? ? super : instance.update!(tweet)
+    def ==(other)
+      other.is_a?(self.class) && id == other.id
+    end
+
+    def date
+      format = Time.now - @created_at < 86_400 ? '%H:%M:%S' : '%Y-%m-%d %H:%M:%S'
+      @created_at.strftime(format)
+    end
+
+    def expand_url!
+      sub = -> (x) { @text.sub!(x.url, x.display_url) }
+      (@media + @urls).each(&sub)
+    end
+
+    def favorite!
+      @favorite_count += 1
+      @favorited = true
+    end
+
+    def grepped_with?(query)
+      [text, user.screen_name, user.name]
+        .any? { |x| x.downcase.include?(query.downcase) }
+    end
+
+    def in_reply_to_status(&block)
+      if @in_reply_to_status_id.nil?
+        block.call(nil)
+        return
+      end
+
+      status = Status.find(@in_reply_to_status_id)
+      unless status.nil?
+        block.call(status)
+        return
+      end
+
+      Client.current.show_status(@in_reply_to_status_id, &block)
     end
 
     def initialize(tweet)
@@ -51,6 +87,32 @@ module Twterm
       @@instances[id] = self
     end
 
+    def replies
+      Status.all.select { |s| s.in_reply_to_status_id == id }
+    end
+
+    def retweet!
+      @retweet_count += 1
+      @retweeted = true
+    end
+
+    def retweeted_by
+      User.find(@retweeted_by_user_id)
+    end
+
+    def split(width)
+      @splitted_text[:width] ||= @text.split_by_width(width)
+    end
+
+    def touch!
+      @touched_at = Time.now
+    end
+
+    def unfavorite!
+      @favorite_count -= 1
+      @favorited = false
+    end
+
     def update!(tweet)
       @retweet_count = tweet.retweet_count
       @favorite_count = tweet.favorite_count
@@ -59,104 +121,42 @@ module Twterm
       self
     end
 
-    def date
-      format = Time.now - @created_at < 86_400 ? '%H:%M:%S' : '%Y-%m-%d %H:%M:%S'
-      @created_at.strftime(format)
-    end
-
-    def expand_url!
-      sub = -> (x) { @text.sub!(x.url, x.display_url) }
-      (@media + @urls).each(&sub)
-    end
-
-    def favorite!
-      @favorite_count += 1
-      @favorited = true
-    end
-
-    def unfavorite!
-      @favorite_count -= 1
-      @favorited = false
-    end
-
-    def retweet!
-      @retweet_count += 1
-      @retweeted = true
-    end
-
-    def split(width)
-      @splitted_text[:width] ||= @text.split_by_width(width)
-    end
-
-    def in_reply_to_status(&block)
-      if @in_reply_to_status_id.nil?
-        block.call(nil)
-        return
-      end
-
-      status = Status.find(@in_reply_to_status_id)
-      unless status.nil?
-        block.call(status)
-        return
-      end
-
-      Client.current.show_status(@in_reply_to_status_id, &block)
-    end
-
-    def replies
-      Status.all.select { |s| s.in_reply_to_status_id == id }
-    end
-
-    def retweeted_by
-      User.find(@retweeted_by_user_id)
-    end
-
-    def touch!
-      @touched_at = Time.now
-    end
-
     def user
       User.find(user_id)
     end
 
-    def grepped_with?(query)
-      [text, user.screen_name, user.name]
-        .any? { |x| x.downcase.include?(query.downcase) }
+    def self.all
+      @@instances.values
     end
 
-    def ==(other)
-      other.is_a?(self.class) && id == other.id
+    def self.cleanup
+      TabManager.instance.each_tab do |tab|
+        tab.touch_statuses if tab.is_a?(Tab::StatusesTab)
+      end
+      cond = -> (status) { status.touched_at > Time.now - MAX_CACHED_TIME }
+      statuses = all.select(&cond)
+      status_ids = statuses.map(&:id)
+      @@instances = status_ids.zip(statuses).to_h
     end
 
-    class << self
-      def all
-        @@instances.values
-      end
+    def self.find(id)
+      @@instances[id]
+    end
 
-      def find(id)
-        @@instances[id]
-      end
+    def self.find_or_fetch(id)
+      instance = find(id)
+      (yield(instance) && return) if instance
 
-      def find_or_fetch(id)
-        instance = find(id)
-        (yield(instance) && return) if instance
+      Client.current.show_status(id) { |status| yield status }
+    end
 
-        Client.current.show_status(id) { |status| yield status }
-      end
+    def self.new(tweet)
+      instance = find(tweet.id)
+      instance.nil? ? super : instance.update!(tweet)
+    end
 
-      def parse_time(time)
-        (time.is_a?(String) ? Time.parse(time) : time.dup).localtime
-      end
-
-      def cleanup
-        TabManager.instance.each_tab do |tab|
-          tab.touch_statuses if tab.is_a?(Tab::StatusesTab)
-        end
-        cond = -> (status) { status.touched_at > Time.now - MAX_CACHED_TIME }
-        statuses = all.select(&cond)
-        status_ids = statuses.map(&:id)
-        @@instances = status_ids.zip(statuses).to_h
-      end
+    def self.parse_time(time)
+      (time.is_a?(String) ? Time.parse(time) : time.dup).localtime
     end
   end
 end
