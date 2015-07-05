625b1a4970175d21fa82fa7ebe2c25f8beca771f
Ryota Kameoka <kameoka.ryota@gmail.com>
2015-07-05 18:59:45 +0900

    Refactor Tab::Base

--- lib/twterm/tab/base.rb
+++ lib/twterm/tab/base.rb
@@ -5,33 +5,43 @@ module Twterm
 
       attr_accessor :title
 
-      def initialize
-        @window = stdscr.subwin(stdscr.maxy - 5, stdscr.maxx - 30, 3, 0)
-      end
-
-      def refresh
-        return if @refreshing || closed? || TabManager.instance.current_tab.object_id != object_id
-
-        @refreshing = true
-        Thread.new do
-          update
-          @refreshing = false
-        end
+      def ==(other)
+        self.equal?(other)
       end
 
       def close
         @window.close
       end
 
+      def initialize
+        @window = stdscr.subwin(stdscr.maxy - 5, stdscr.maxx - 30, 3, 0)
+      end
+
+      def refresh
+        return unless refreshable?
+
+        Thread.new do
+          refresh_mutex.synchronize { update }
+        end
+      end
+
       def respond_to_key(_)
         fail NotImplementedError, 'respond_to_key method must be implemented'
       end
 
-      def ==(other)
-        self.equal?(other)
+      private
+
+      def refresh_mutex
+        @refresh_mutex ||= Mutex.new
       end
 
-      private
+      def refreshable?
+        !(
+          refresh_mutex.locked? ||
+            closed? ||
+            TabManager.instance.current_tab.object_id != object_id
+        )
+      end
 
       def update
         fail NotImplementedError, 'update method must be implemented'
