afc64210bbc32437f0e0635663cd075f8044d580
Ryota Kameoka <kameoka.ryota@gmail.com>
2017-04-15 23:43:20 +0900

    Complete missing comma

--- lib/twterm/tab/dumpable.rb
+++ lib/twterm/tab/dumpable.rb
@@ -2,7 +2,7 @@ module Twterm
   module Tab
     module Dumpable
       def dump
-        fail NotImplementedError 'dump method must be implemented'
+        fail NotImplementedError, 'dump method must be implemented'
       end
 
       def self.included(klass)
