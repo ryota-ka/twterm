4a7353bd557d74c62b7f182057e17b1870deb80f
Ryota Kameoka <kameoka.ryota@gmail.com>
2017-07-20 02:44:23 +0900

    Define fixture function in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,11 @@ RSpec.configure do |config|
   config.order = :random
 end
 
+def fixture(path)
+  dir = File.expand_path('../fixtures', __FILE__)
+  File.read("#{dir}/#{path}")
+end
+
 Dir['spec/supports/**/*.rb'].each { |f| require File.expand_path(f, __dir__ + '/..') }
 
 require 'twterm'
