a364f08e31a511002e4b276de47a705e01c54b6c
Ryota Kameoka <kameoka.ryota@gmail.com>
2017-07-29 03:04:24 +0900

    Rewrite image/between.rb in require-context-safe way

--- lib/twterm/image/between.rb
+++ lib/twterm/image/between.rb
@@ -1,29 +1,31 @@
-class Twterm::Image::Between < Twterm::Image
-  def initialize(image, open, close)
-    @image, @open, @close = image, open, close
+class Twterm::Image
+  class Between < Twterm::Image
+    def initialize(image, open, close)
+      @image, @open, @close = image, open, close
+    end
+
+    def height
+      1
+    end
+
+    def render(window)
+      open.at(line, column).render(window)
+      image.at(line, column + open.width).render(window)
+      close.at(line, column + open.width + image.width).render(window)
+
+      self
+    end
+
+    def to_s
+      "#{open}#{image}#{close}"
+    end
+
+    def width
+      [open, image, close].map(&:width).reduce(0, :+)
+    end
+
+    private
+
+    attr_reader :open, :close, :image
   end
-
-  def height
-    1
-  end
-
-  def render(window)
-    open.at(line, column).render(window)
-    image.at(line, column + open.width).render(window)
-    close.at(line, column + open.width + image.width).render(window)
-
-    self
-  end
-
-  def to_s
-    "#{open}#{image}#{close}"
-  end
-
-  def width
-    [open, image, close].map(&:width).reduce(0, :+)
-  end
-
-  private
-
-  attr_reader :open, :close, :image
 end
